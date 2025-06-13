2e153bc5ff7f3827b202b6fd15ff48bd57bace4d
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-12 23:22:03 -0600

    Update TestPelicula.cpp

--- TestPelicula.cpp
+++ TestPelicula.cpp
@@ -3,7 +3,7 @@
 
 TEST(PeliculaTest1, ConstructorYSetGet)
 {
-Pelicula p("El cardenal",159,"Drama")
+Pelicula p(11182,"El cardenal",159,"Drama")
     EXPECT_EQ(p.getTitulo(), "El cardenal");
     EXPECT_EQ(p.getDuracion(), 159);
     EXPECT_EQ(p.getGenero(), "Drama");
@@ -12,14 +12,30 @@ Pelicula p("El cardenal",159,"Drama")
 
 TEST(PeliculaTest2, ProbarCalcularPromedio)
 {
-    Pelicula p("El marciano", 165, "Misterio");
+    Pelicula p(342,"El marciano", 165, "Misterio");
     p.agregarCalificacion(4);
     p.agregarCalificacion(5);
 
     EXPECT_DOUBLE_EQ(p.calcularPromedio(), 4.5);
 
 }
+
 TEST(PeliculaTest3, PromedioSinCalificacionesLanzaExcepcion) {
-    Pelicula p("El marciano", 100, "Drama");
+    Pelicula p(9024,"El marciano 2", 100, "Drama");
     EXPECT_THROW(p.calcularPromedio(), const char*);
+}
+
+TEST(PeliculaTest4, ProbarMetodoMostrar){
+    Pelicula p(1342,"El cardenal",159,"Drama");
+    std::stringstream flujo;
+
+    p.mostrar(flujo);
+
+    std::string esperado;
+    esperado<<"Pelicula: " << p.getNombre() << std::endl <<
+	"Duracion: " << p.getDuracion() << std::endl <<
+	"Genero: " << p.getGenero() << std::endl <<
+	"ID: " << p.getId() << std::endl <<
+	"Calificacion Promedio: " << p.calcularPromedio() << std::endl;
+    EXPECT_EQ(flujo.str(), esperado);
 }
