8a0dde4f84f0382ca222def97580b6a37a412c89
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-13 14:01:15 -0600

    Update Pelicula.cpp

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,3 +1,11 @@
+
+/*
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #include "Pelicula.h"
 // Constructor, esta subclase solo tiene de atributos los de la clase base Video
 Pelicula::Pelicula(int id_, const std::string& nombre_, double duracion_,  const std::string& genero_)
