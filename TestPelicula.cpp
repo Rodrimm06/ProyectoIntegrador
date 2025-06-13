f344725edf0e0b5db7def17127c1556a331bbde1
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-12 22:10:19 -0600

    Update TestPelicula.cpp

--- TestPelicula.cpp
+++ TestPelicula.cpp
@@ -1,17 +1,25 @@
 #include <gtest/gtest.h>
 #include "Pelicula.h"
 
-TEST(Pelicula, ConstructorYSetGet)
+TEST(PeliculaTest1, ConstructorYSetGet)
 {
 Pelicula p("El cardenal",159,"Drama")
     EXPECT_EQ(p.getTitulo(), "El cardenal");
     EXPECT_EQ(p.getDuracion(), 159);
     EXPECT_EQ(p.getGenero(), "Drama");
+    
 }
 
-TEST(DivTest, HandlesZeroInput)
+TEST(PeliculaTest2, ProbarCalcularPromedio)
 {
-int result = divide(9, 0);
+    Pelicula p("El marciano", 165, "Misterio");
+    p.agregarCalificacion(4);
+    p.agregarCalificacion(5);
 
-EXPECT_EQ(result, 0);
+    EXPECT_DOUBLE_EQ(p.calcularPromedio(), 4.5);
+
+}
+TEST(PeliculaTest3, PromedioSinCalificacionesLanzaExcepcion) {
+    Pelicula p("El marciano", 100, "Drama");
+    EXPECT_THROW(p.calcularPromedio(), const char*);
 }
