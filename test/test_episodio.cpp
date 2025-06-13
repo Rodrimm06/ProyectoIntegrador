8c3f0f5fca5abc3a2b033f5adff62dfe4ebe66b9
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-13 13:03:21 -0600

    Update test_episodio.cpp

--- test/test_episodio.cpp
+++ test/test_episodio.cpp
@@ -3,7 +3,8 @@
 
 TEST(EpisodioTest1, ConstructorExcepcion)
 {
-    EXPECT_THROW({Episodio e("Piloto",-11)}, const char*);
+    // Quitar las llaves {} en EXPECT_THROW
+    EXPECT_THROW(Episodio("Piloto",-11), const char*);
 }
 
 TEST(EpisodioTest2, ProbarCalcularPromedio)
@@ -13,7 +14,6 @@ TEST(EpisodioTest2, ProbarCalcularPromedio)
     e.agregarCalificacion(5);
 
     EXPECT_DOUBLE_EQ(e.calcularPromedio(), 4.5);
-
 }
 
 TEST(EpisodioTest3, PromedioSinCalificacionesLanzaExcepcion) {
@@ -24,19 +24,25 @@ TEST(EpisodioTest3, PromedioSinCalificacionesLanzaExcepcion) {
 TEST(EpisodioTest4, ProbarMetodoMostrar){
     Episodio e("Paloma",18);
     e.agregarCalificacion(3);
-    std::stringstream flujo;
 
-    e.mostrar(flujo);
+    std::stringstream flujo;
+    std::streambuf* oldCout = std::cout.rdbuf(flujo.rdbuf());
+
+    e.mostrar();
+
+    std::cout.rdbuf(oldCout);
 
     std::stringstream esperado;
-    esperado<<"Episodio: " << std::endl <<
-    "Nombre: "<<"Paloma" << std::endl <<
-	  "Temporada: " << 18 << std::endl <<
-	  "Calificacion Promedio: " << 3 << std::endl;
+    esperado << "Episodio: " << std::endl
+             << "Nombre: " << "Paloma" << std::endl
+             << "Temporada: " << 18 << std::endl
+             << "Calificacion Promedio: " << 3 << std::endl;
+
     EXPECT_EQ(flujo.str(), esperado.str());
 }
 
 TEST(EpisodioTest5, ProbarAgregarCalificacionErronea){
-		Episodio e("Jamaica",3);
-		EXPECT_THROW(e.agregarCalificacion(-23),const char*);
-	}
+    Episodio e("Jamaica",3);
+    EXPECT_THROW(e.agregarCalificacion(-23), const char*);
+}
+
