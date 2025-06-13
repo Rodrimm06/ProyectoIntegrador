04d2e9f3dcca69dd383daaf13abbcf48bb5950b8
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-13 14:01:42 -0600

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -1,3 +1,11 @@
+
+/*
+ * Copyright (C) 2025 Tec de Monterrey
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 #include "Video.h"
 // Constructor
 Video::Video(int id_, const std::string& nombre_, double duracion_, const std::string& genero_):id(id_),nombre(nombre_),
