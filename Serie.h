24c814d845dd30bda0ca29c1ef12bba74bcf6571
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-14 11:58:50 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -18,19 +18,33 @@ class Serie: public Video
 private:
 	std::vector<Episodio> episodios;
 public:
-	// Constructor
+	/** 
+        Constructor
+        */
 	Serie(int id_, const std::string& nombre_, double duracion_, const std::string& genero_);
-	// Para agregar episodios
+	/** 
+        Para agregar episodios
+        */
 	void agregarEpisodio(const Episodio& episodio);
-	// Para visualizar todos los episodios
+	/**
+        Para visualizar todos los episodios
+        */
 	void mostrarEpisodios() const;
-	// Funcion de mostrar
+	/** 
+        Funcion de mostrar
+        */
 	void mostrar() const override;
-	// Para sacar el promedio de toda la serie
+	/** 
+        Para sacar el promedio de toda la serie
+        */
 	double calcularPromedio() const override;
-	// Sobrecarga de operador para mayor eficiencia
+	/** 
+        Sobrecarga de operador para mayor eficiencia
+        */
 	friend std::ostream& operator<<(std::ostream& os, const Serie& serie);
-	// Funcion para obtener episodio
+	/** 
+        Funcion para obtener episodio
+        */
         const std::vector<Episodio>& getEpisodios() const { return episodios; }
 
 };
