1308b116e8798be7683a3a792d7b7a861f309904
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-14 10:40:53 -0600

    Update Pelicula.cpp

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -6,6 +6,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 #include "Pelicula.h"
 // Constructor, esta subclase solo tiene de atributos los de la clase base Video
 Pelicula::Pelicula(int id_, const std::string& nombre_, double duracion_,  const std::string& genero_)
