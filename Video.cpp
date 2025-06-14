b2d6f75a70104fae4cdc373eb0f7cdf3a3714f5b
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-14 13:28:16 -0600

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2025 Tec de Monterrey
  *
@@ -8,63 +7,63 @@
  */
 
 #include "Video.h"
+
 // Constructor
-Video::Video(int id_, const std::string& nombre_, double duracion_, const std::string& genero_):id(id_),nombre(nombre_),
-duracion(duracion_),genero(genero_){
-	if (genero != "Misterio" && genero != "Accion" && genero != "Drama") {
-		throw "Error, genero no reconocido";
-	}
-	if (duracion <= 0) {
-		throw "Error, duracion invalida";
-	}
-}
-// Funcion para agregar una calificacion al vector
-void Video::AgregarCalificacion(int cali) {
-	if (cali > 0 && cali < 6) {
-		calificaciones.push_back(cali);
-	}
-	else {
-		throw "Error, calificacion fuera de rango";
-	}
-}
-// Funcion para calcular el promedio
-double Video::CalcularPromedio() const{
-	double promedio=0;
-	// For para recorrer el vector de calificaciones
-	for (int i = 0;i < calificaciones.size();i++) {
-		promedio = promedio + calificaciones[i];
-	}
-	if (calificaciones.size() > 0) {
-		return promedio / calificaciones.size();
-	}
-	else {
-		throw "Error, no se puede promediar sin calificaciones";
-	}
+Video::Video(int id_, const std::string& nombre_, double duracion_, const std::string& genero_)
+    : id(id_), nombre(nombre_), duracion(duracion_), genero(genero_) {
+    if (genero != "Misterio" && genero != "Accion" && genero != "Drama") {
+        throw "Error, genero no reconocido";
+    }
+    if (duracion <= 0) {
+        throw "Error, duracion invalida";
+    }
 }
 
+// Funcion para agregar una calificacion al vector
+void Video::AgregarCalificacion(int cali) {
+    if (cali > 0 && cali < 6) {
+        calificaciones.push_back(cali);
+    } else {
+        throw "Error, calificacion fuera de rango";
+    }
+}
+
+// Funcion para calcular el promedio
+double Video::CalcularPromedio() const {
+    double promedio = 0;
+    // For para recorrer el vector de calificaciones
+    for (int i = 0; i < calificaciones.size(); i++) {
+        promedio = promedio + calificaciones[i];
+    }
+    if (calificaciones.size() > 0) {
+        return promedio / calificaciones.size();
+    } else {
+        throw "Error, no se puede promediar sin calificaciones";
+    }
+}
 
 // Funciones set/get
 void Video::SetId(int id_) {
-	id = id_;
+    id = id_;
 }
 void Video::SetNombre(std::string nom) {
-	nombre = nom;
+    nombre = nom;
 }
 void Video::SetDuracion(double dur) {
-	duracion = dur;
+    duracion = dur;
 }
 void Video::SetGenero(std::string gen) {
-	genero = gen;
+    genero = gen;
 }
 int Video::GetId() const {
-	return id;
+    return id;
 }
 std::string Video::GetNombre() const {
-	return nombre;
+    return nombre;
 }
 double Video::GetDuracion() const {
-	return duracion;
+    return duracion;
 }
 std::string Video::GetGenero() const {
-	return genero;
+    return genero;
 }
