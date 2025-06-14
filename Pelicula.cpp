6e916931f81fff730402958b388d2c12d4856d93
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-14 13:26:59 -0600

    Update Pelicula.cpp

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2025 Tec de Monterrey
  *
@@ -10,23 +9,23 @@
 #include "Pelicula.h"
 // Constructor, esta subclase solo tiene de atributos los de la clase base Video
 Pelicula::Pelicula(int id_, const std::string& nombre_, double duracion_,  const std::string& genero_)
-	:Video(id_,nombre_,duracion_,genero_){
-	
+    : Video(id_, nombre_, duracion_, genero_) {
+    
 }
 // Funcion para mostrar informacion de la pelicula
-void Pelicula::	Mostrar() const {
-	std::cout << "Pelicula: " << GetNombre() << std::endl <<
-		"Duracion: " << GetDuracion() << std::endl <<
-		"Genero: " << GetGenero() << std::endl <<
-		"ID: " << GetId() << std::endl <<
-		"Calificacion Promedio: " << CalcularPromedio() << std::endl;
+void Pelicula::Mostrar() const {
+    std::cout << "Pelicula: " << GetNombre() << std::endl <<
+        "Duracion: " << GetDuracion() << std::endl <<
+        "Genero: " << GetGenero() << std::endl <<
+        "ID: " << GetId() << std::endl <<
+        "Calificacion Promedio: " << CalcularPromedio() << std::endl;
 }
 // Sobrecarga de operadores para mayor eficiencia
 std::ostream& operator<<(std::ostream& os, const Pelicula& pelicula) {
-	os << "Pelicula: " << pelicula.GetNombre() << std::endl
-		<< "Duracion: " << pelicula.GetDuracion() << std::endl
-		<< "Genero: " << pelicula.GetGenero() << std::endl
-		<< "ID: " << pelicula.GetId() << std::endl
-		<< "Calificacion Promedio: " << pelicula.CalcularPromedio() << std::endl;
-	return os;
+    os << "Pelicula: " << pelicula.GetNombre() << std::endl
+        << "Duracion: " << pelicula.GetDuracion() << std::endl
+        << "Genero: " << pelicula.GetGenero() << std::endl
+        << "ID: " << pelicula.GetId() << std::endl
+        << "Calificacion Promedio: " << pelicula.CalcularPromedio() << std::endl;
+    return os;
 }
