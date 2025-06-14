8b57267d274fea9daa78de38ec58360d9cd1398a
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-14 11:16:10 -0600

    Update Episodio.cpp

--- Episodio.cpp
+++ Episodio.cpp
@@ -16,12 +16,12 @@ Episodio::Episodio(const std::string& nombre_, int temporada_) :
     }
 }
 // Se hace la funcion mostrar para ver la informacion de cada episodio
-void Episodio::mostrarEpisodio() const {
+void Episodio::MostrarEpisodio() const {
     std::cout << "Episodio: " << std::endl << "Nombre: " << nombre << std::endl <<
         "Temporada: " << temporada << std::endl << "Calificacion Promedio: " << calcularPromedioEpisodio() << std::endl;
 }
 // Funcion para agregar una calificacion al vector de calificaciones
-void Episodio::agregarCalificacionEpisodio(int cali) {
+void Episodio::AgregarCalificacionEpisodio(int cali) {
     // Se asegura que la calificacion este dentro del rango
     if (cali > 0 && cali < 6) {
         calificaciones.push_back(cali);
@@ -31,7 +31,7 @@ void Episodio::agregarCalificacionEpisodio(int cali) {
     }
 }
 // Para calcular el promedio de calificaciones
-double Episodio::calcularPromedioEpisodio() const {
+double Episodio::CalcularPromedioEpisodio() const {
     double promedio = 0;
     // For para recorrer el vector de calificaciones
     for (int i = 0; i < calificaciones.size(); i++) {
