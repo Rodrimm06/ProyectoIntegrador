2e2b823086028aa08b0ec16db3643c43b23e0059
Rodrimm06 <rodrimm2006@gmail.com>
2025-06-15 10:03:43 -0600

    Update test_serie.cpp

--- test/test_serie.cpp
+++ test/test_serie.cpp
@@ -193,14 +193,14 @@ TEST(SerieTest15, CoberturaMetodosUsadosEnMostrar) {
     Serie s(5678, "Don gato", 45, "Drama");
 
     Episodio e("Inicio", 1);
-    e.AgregarCalificacionEpisodio(5)
-    s.AgregarEpisodio(e);        // necesario para que CalcularPromedio funcione
+    e.AgregarCalificacionEpisodio(5);
+    s.AgregarEpisodio(e);
 
     EXPECT_EQ(s.GetNombre(), "Serie Ejemplo");
     EXPECT_EQ(s.GetDuracion(), 45);
     EXPECT_EQ(s.GetGenero(), "Acción");
     EXPECT_EQ(s.GetId(), 5678);
-    EXPECT_DOUBLE_EQ(s.CalcularPromedio(), 5.0);  // promedio de un episodio calificado con 5
+    EXPECT_DOUBLE_EQ(s.CalcularPromedio(), 5.0);
 
     const std::vector<Episodio>& episodios = s.GetEpisodios();
     ASSERT_EQ(episodios.size(), 1);
